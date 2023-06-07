20a90f2d6bbfb3ec096f63ff59ab777527ff6868
wakamejiru <kazunabe0150@gmail.com>
2023-06-07 22:05:57 +0900

    コメント追加
    解説用のコメントを追加

--- Nazepedia/app/src/main/java/com/example/nazepedia/MainActivity.java
+++ Nazepedia/app/src/main/java/com/example/nazepedia/MainActivity.java
@@ -13,23 +13,31 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.nazepedia.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    // バインディング　レイアウトの変数をレイアウト内のビューに関連付ける
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        // インスタンスを受け取る
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // bindingのRootの画面を表示する
         setContentView(binding.getRoot());
 
+        // ナビゲーションの下画面を登録
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
+        //　ナビゲーションバーによって変更された時に、対応したフラグメントで表示が変更される。
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
+
+        // ナビゲーションバーの項目をコントローラーに紐づける
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
+        // アクションバーとしてナビゲーションのコントローラーと表示を紐づける
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
