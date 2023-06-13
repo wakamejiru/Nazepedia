7c129f72c42ed4aba8bf91df238a471e898a950e
wakamejiru <kazunabe0150@gmail.com>
2023-06-13 22:02:48 +0900

    ロードが止まりない問題を修正&高速化

--- Nazepedia/app/src/main/java/com/example/nazepedia/ui/bbs/BbsFragment.java
+++ Nazepedia/app/src/main/java/com/example/nazepedia/ui/bbs/BbsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.TextView;
 
@@ -16,6 +17,7 @@ import com.example.nazepedia.databinding.FragmentBbsBinding;
 public class BbsFragment extends Fragment {
 
     private FragmentBbsBinding binding;
+    private static final String Keijibvann_URL = "https://nazeru.jimdofree.com/%E9%97%87%E3%81%AE%E6%8E%B2%E7%A4%BA%E6%9D%BF/";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,13 +29,12 @@ public class BbsFragment extends Fragment {
 
         // WebViewを表示
         final WebView webView = binding.bbsView;
-        String Keijibvann_URL = "https://nazeru.jimdofree.com/%E9%97%87%E3%81%AE%E6%8E%B2%E7%A4%BA%E6%9D%BF/";
-        webView.loadUrl(Keijibvann_URL);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // JavaScriptを有効化する
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT); // キャッシュを使用する
+        webSettings.setDomStorageEnabled(true); // DOMストレージを有効化する
 
-        /*
-        final TextView textView = binding.textDashboard;
-        bbsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        */
+        webView.loadUrl(Keijibvann_URL);
         return root;
     }
 
